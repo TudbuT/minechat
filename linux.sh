230b3ead60dbee65a6dbb494e37697b54f210117
TudbuT <48156391+TudbuT@users.noreply.github.com>
2019-11-23 19:56:45 +0000

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -1,11 +1,11 @@
 echo -e "\e[48;5;10m\e[38;5;0m[i]\e[0m Installing/Updating NodeJS and npm"
-yes Y | apt-get update>ilog.txt 2>&1
-yes Y | apt-get install npm>>ilog.txt 2>&1
-yes Y | apt-get install nodejs>>ilog.txt 2>&1
+#yes Y | apt-get update>ilog.txt 2>&1
+#yes Y | apt-get install npm>>ilog.txt 2>&1
+#yes Y | apt-get install nodejs>>ilog.txt 2>&1
 echo -e "\e[48;5;10m\e[38;5;0m[i]\e[0m Installing/Updating libraries"
-npm install n>>ilog.txt 2>&1
-npm install -g npm>>ilog.txt 2>&1
-npm install minecraft-protocol>>ilog.txt 2>&1
+#npm install n>>ilog.txt 2>&1
+#npm install -g npm>>ilog.txt 2>&1
+#npm install minecraft-protocol>>ilog.txt 2>&1
 echo -e "\e[48;5;10m\e[38;5;0m[i]\e[0m Searching for updates"
 git config user.email "x@y.z"
 git config user.name "XYZ"
