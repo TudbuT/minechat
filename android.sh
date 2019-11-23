50d30110a14a83dc16c6dfd509716f5c78eab877
TudbuT <48156391+TudbuT@users.noreply.github.com>
2019-11-23 19:37:26 +0000

    Create android.sh

---
+++ android.sh
@@ -0,0 +1,17 @@
+echo -e "\e[48;5;10m\e[38;5;0m[i]\e[0m Installing/Updating NodeJS and npm"
+yes Y | apt-get update>ilog.txt 2>&1
+yes Y | apt-get install npm>>ilog.txt 2>&1
+yes Y | apt-get install nodejs>>ilog.txt 2>&1
+yes Y | pkg update>>ilog.txt 2>&1
+yes Y | pkg install npm>>ilog.txt 2>&1
+yes Y | pkg install nodejs>>ilog.txt 2>&1
+echo -e "\e[48;5;10m\e[38;5;0m[i]\e[0m Installing/Updating libraries"
+npm install n>>ilog.txt 2>&1
+npm install minecraft-protocol
+echo -e "\e[48;5;10m\e[38;5;0m[i]\e[0m Searching for updates"
+git config user.email "y@x.z"
+git config user.name "XYZ"
+git pull https://github.com/tudbut/minechat.git>>ilog.txt 2>&1
+echo -e "\e[48;5;10m\e[38;5;0m[i]\e[0m All libraries should be installed now"
+echo -e "\e[48;5;10m\e[38;5;0m[i]\e[0m Starting!"
+node main.js
