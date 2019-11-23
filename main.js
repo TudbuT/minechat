a88d09e1fe12ac471599ddf572d97635c8711c77
TudbuT <48156391+TudbuT@users.noreply.github.com>
2019-11-23 17:45:59 +0100

    Create main.js

---
+++ main.js
@@ -0,0 +1,29 @@
+const mc = require("minecraft-protocol")
+const server = require("./server.js")
+const data = require("./data.json")
+
+var client = mc.createClient({
+  host: data.server.ip,
+  port: data.server.port,
+  username: data.email,
+  password: data.password,
+});
+
+module.exports = {
+  chat: [
+     
+  ],
+  sendChat: function (msg) {
+    client.write('chat', {message: msg});
+  }
+}
+
+client.on('chat', function(packet) {
+  var jsonMsg = JSON.parse(packet.message);
+  if(jsonMsg.translate == 'chat.type.announcement' || jsonMsg.translate == 'chat.type.text') {
+    var username = jsonMsg.with[0].text;
+    var msg = jsonMsg.with[1];
+    module.exports.chat[module.exports.chat.length] = "<" + username + "> " + msg
+    server.newChat("<" + username + "> " + msg);
+  }
+});
