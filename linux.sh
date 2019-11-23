39a919986cd8bb82e18ba0479e9a6371bbfc8ae7
TudbuT <48156391+TudbuT@users.noreply.github.com>
2019-11-23 19:42:14 +0000

    Update linux.sh

--- linux.sh
+++ linux.sh
@@ -11,7 +11,7 @@ git config user.email "x@y.z"
 git config user.name "XYZ"
 git pull https://github.com/tudbut/minechat.git>>ilog.txt 2>&1
 echo -e "\e[48;5;10m\e[38;5;0m[i]\e[0m All libraries should be installed now"
-echo -e "\e[48;5;10m\e[38;5;0m[i]\e[0m Creating terminal command 'bottyclient' (this requires administrator)"
+echo -e "\e[48;5;10m\e[38;5;0m[i]\e[0m Creating terminal command 'minechat' (this requires administrator)"
 sudo echo `cd ${PWD}`>/bin/minechat
 sudo echo `bash run.sh`>>/bin/minechat
 sudo chmod +x /bin/minechat
