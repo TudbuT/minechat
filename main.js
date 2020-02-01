c9e772c7878bebafe788a777005c4f91bc8bd195
Daniel H <dh.danielhister@gmail.com>
2020-02-01 16:24:20 +0100

    Update main.js

--- main.js
+++ main.js
@@ -51,7 +51,7 @@ process.stdin.on("data", (key) => {
   if(!free) return;
   
   process.stdout.write(key);
-  if(key === "\x13") {
+  if(key === "\x08") {
     message = "";
     for (let chr in m) {
       if(chr !== m.length - 2)
