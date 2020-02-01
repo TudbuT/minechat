0da312e29b86123bd1f5937ffe29e3d71bde215c
Daniel H <dh.danielhister@gmail.com>
2020-02-01 17:11:08 +0100

    Update main.js

--- main.js
+++ main.js
@@ -50,7 +50,7 @@ process.stdin.on("data", (key) => {
   
   if(!free) return;
   
-  if(key === "<") {
+  if(key === "\x08") {
     let emessage = message.slice(0, message.length - 2);
     m = emessage.split("");
     process.stdout.write("\r\x1b[K")
