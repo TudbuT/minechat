241bf2a930d2e781e415feb09beb1bf99515b0d1
Daniel H <dh.danielhister@gmail.com>
2020-02-01 16:37:12 +0100

    Update main.js

--- main.js
+++ main.js
@@ -50,7 +50,7 @@ process.stdin.on("data", (key) => {
   
   if(!free) return;
   
-  if(key === "\x1b[3~") {
+  if(key === "\b\x1b[3~") {
     message = "";
     for (let chr in m) {
       if(chr !== m.length - 3)
