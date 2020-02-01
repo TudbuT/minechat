571be2294b7f9611f7d49a21f57f25697f6050d7
Daniel H <dh.danielhister@gmail.com>
2020-02-01 15:57:45 +0100

    Update main.js

--- main.js
+++ main.js
@@ -100,7 +100,7 @@ client.on("error", (err) => {
   for (let chr of m) {
     message += "" + chr;
   }
-  console.warn (err);
+  console.warn ("\n" + err);
   //require("child_process").exec(`echo \`${err.toString().replace("`", "\\`").replace("$", "\\$")}\`>>errors.txt`)
   process.stdout.write("\n>>> " + message)
 })
